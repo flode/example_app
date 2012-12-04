f5d4e44ba64aaea9e01a7ea9e1ea265792d8edc4
Florian Scheibner <flo.de@hotmail.de>
2012-12-04 14:18:32 +0100

    layout, fullTitle

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,7 @@ describe "Static pages", :type => :controller do
     it "should have the content 'Example App'" do
       visit '/static_pages/home'
       page.should have_content('Example App')
+      page.should have_title(full_title(""))
     end
   end
 
